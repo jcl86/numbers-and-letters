d2a73cbed7018d7b98f04fcf946a9805dae985b9
jcl86 <jorgecano86@gmail.com>
2020-06-07 21:23:04 +0200

    Add letters functionality wip

---
+++ src/Calculator/Domain/Letters/Letter.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator
+{
+    public class Letter
+    {
+        private readonly char letter;
+        public bool IsUsed { get; private set; }
+        public void Use() => IsUsed = true;
+        public void Reset() => IsUsed = false;
+
+        public Letter(char letter)
+        {
+            this.letter = letter;
+            IsUsed = false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            if (obj is Letter other)
+            {
+                return other.letter.Equals(letter);
+            }
+            return false;
+        }
+
+        public override int GetHashCode() => letter.GetHashCode();
+        public override string ToString() => letter.ToString().ToUpper();
+    }
+}
--- src/Calculator/Domain/Letters/LettersGame.cs
+++ src/Calculator/Domain/Letters/LettersGame.cs
@@ -1,14 +1,54 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Calculator
 {
+    public class AvaliableLetters
+    {
+        public IEnumerable<Letter> Letters { get; private set; }
+
+        public AvaliableLetters(IEnumerable<char> letters)
+        {
+            Letters = letters.Select(x => new Letter(x));
+        }
+
+        public bool FormWord(string word)
+        {
+            bool canBeFormed = true;
+            Reset();
+            foreach (var character in word)
+            {
+                var foundLetter = Letters
+                    .Where(x => !x.IsUsed)
+                    .FirstOrDefault(x => x.Equals(new Letter(character)));
+
+                if (foundLetter is null)
+                {
+                    canBeFormed = false;
+                }
+
+                foundLetter?.Use();
+            }
+
+            return canBeFormed;
+        }
+
+        private void Reset()
+        {
+            foreach (var letter in Letters)
+            {
+                letter.Reset();
+            }
+        }
+    }
+
     public class LettersGame
     {
-        public IEnumerable<char> Letters { get; }
+        public AvaliableLetters AvaliableLetters { get; }
 
         public LettersGame(LettersGamePreparer gamePreparer)
         {
-            Letters = gamePreparer.CreateDeck();
+            AvaliableLetters = new AvaliableLetters(gamePreparer.CreateDeck());
         }
     }
 }
